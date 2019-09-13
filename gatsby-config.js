208fe1050318516fd38da4b3d00d6ebae5150652
Yuki Hirayama <yhirayama20188.itn@gmail.com>
2019-09-13 13:35:42 +0900

    env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
@@ -25,6 +23,14 @@ module.exports = {
       options: {
         name: `posts`,
         path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
     `gatsby-plugin-sass`,
--- src/examples/Header.js
+++ src/examples/Header.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import {useStaticQuery, graphql} from 'gatsby'
-const getData = graphql`{
-  query {
-    site {
-      siteMetadata{
-        siteTitle: title
-        description
-        author
-        data{
-          age
-        }
-      }
-    }
-  }
-`
+// import React from 'react'
+// import {useStaticQuery, graphql} from 'gatsby'
+// const getData = graphql`{
+//   query {
+//     site {
+//       siteMetadata{
+//         siteTitle: title
+//         description
+//         author
+//         data{
+//           age
+//         }
+//       }
+//     }
+//   }
+// `
 
-const Header = () => {
-  const { site: {siteMetadata: info} } = useStaticQuery(getData)
-  return (
-    <div>
-      {/* <h1>author: {data.site.siteMetadata.author}</h1> */}
-      <h1>author: {info.author}</h1>
-      <h1>title: {info.siteTitle}</h1>
-    </div>
-  )
-}
+// const Header = () => {
+//   const { site: {siteMetadata: info} } = useStaticQuery(getData)
+//   return (
+//     <div>
+//       {/* <h1>author: {data.site.siteMetadata.author}</h1> */}
+//       <h1>author: {info.author}</h1>
+//       <h1>title: {info.siteTitle}</h1>
+//     </div>
+//   )
+// }
 
-export default Header
+// export default Header
